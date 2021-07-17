1779d52ca09b1924d9e4af38e9faec7b00a3161b
ysdy44 <ysdy44@outlook.com>
2021-07-17 17:50:12 +0800

    Add LyricSpan in Lyrics

---
+++ Lyric Maker/Lyrics/LyricSpan.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml.Documents;
+
+namespace Lyric_Maker.Lyrics
+{
+    /// <summary>
+    /// Span of Lyric.
+    /// </summary>
+    public sealed partial class LyricSpan : Span
+    {
+        public Run TimeRun => this._TimeRun;
+        public Run TextRun => this._TextRun;
+
+        //@Construct
+        /// <summary>
+        /// Initializes a LyricSpan. 
+        /// </summary>
+        public LyricSpan()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
