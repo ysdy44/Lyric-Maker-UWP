5e787b1174c8dc6ec789361164a57efdd9917159
ysdy44 <ysdy44@outlook.com>
2022-03-10 03:02:26 +0800

    ThumbSlider replace PositionSlider

--- Lyric Maker/Elements/ThumbSlider.cs
+++ Lyric Maker/Elements/ThumbSlider.cs
@@ -6,9 +6,14 @@ using Windows.UI.Xaml.Controls.Primitives;
 namespace Lyric_Maker.Elements
 {
     /// <summary>
-    /// Represents a Position Slider, it has three events : Started, Delta and Completed.
+    /// Represents a control based on a <see cref="Slider"/>,
+    /// It has four events : 
+    /// <see cref="ThumbSlider.ValueChangedStarted"/>, 
+    /// <see cref="ThumbSlider.ValueChangedDelta"/>, 
+    /// <see cref="ThumbSlider.ValueChangedCompleted"/> 
+    /// and <see cref="ThumbSlider.ValueChangedUnfocused"/>.
     /// </summary>
-    public class PositionSlider : Slider
+    public sealed partial class ThumbSlider : Slider
     {
         //@Delegate
         /// <summary> Occurs when the value changed starts. </summary>
@@ -17,6 +22,11 @@ namespace Lyric_Maker.Elements
         public event RangeBaseValueChangedEventHandler ValueChangedDelta;
         /// <summary> Occurs when the value changed is complete. </summary>
         public event EventHandler ValueChangedCompleted;
+        /// <summary> Occurs when the value changed when Unfocused. </summary>
+        public event RangeBaseValueChangedEventHandler ValueChangedUnfocused;
+
+        bool IsStarted = false;
+        bool IsCompleted = true;
 
         VisualStateGroup CommonStates;
         VisualState Normal;
@@ -29,28 +39,51 @@ namespace Lyric_Maker.Elements
             base.OnApplyTemplate();
 
             //VisualStateGroup
-            if (!(this.CommonStates is null)) this.CommonStates.CurrentStateChanged -= this.CommonStates_CurrentStateChanged;
+            if (this.CommonStates != null) this.CommonStates.CurrentStateChanged -= this.CommonStates_CurrentStateChanged;
             this.CommonStates = base.GetTemplateChild(nameof(CommonStates)) as VisualStateGroup;
-            if (!(this.CommonStates is null)) this.CommonStates.CurrentStateChanged += this.CommonStates_CurrentStateChanged;
+            if (this.CommonStates != null) this.CommonStates.CurrentStateChanged += this.CommonStates_CurrentStateChanged;
             //VisualState
             this.Normal = base.GetTemplateChild(nameof(Normal)) as VisualState;
             this.PointerOver = base.GetTemplateChild(nameof(PointerOver)) as VisualState;
             this.Pressed = base.GetTemplateChild(nameof(Pressed)) as VisualState;
             this.Disabled = base.GetTemplateChild(nameof(Disabled)) as VisualState;
+
+            base.ValueChanged -= this.ValueChangedByKeyboardCore;//Remove Delegate
+            base.ValueChanged += this.ValueChangedByKeyboardCore;//Add Delegate
         }
 
         private void CommonStates_CurrentStateChanged(object sender, VisualStateChangedEventArgs e)
         {
             if (e.OldState != this.Pressed && e.NewState == this.Pressed)
             {
+                this.IsStarted = true;
                 this.ValueChangedStarted?.Invoke(sender, new EventArgs());//Delegate
                 base.ValueChanged += this.ValueChangedDelta;//Add Delegate
             }
+            else
+            {
+                this.IsStarted = false;
+            }
+
             if (e.OldState == this.Pressed && e.NewState != this.Pressed)
             {
+                this.IsCompleted = true;
                 this.ValueChangedCompleted?.Invoke(sender, new EventArgs());//Delegate
                 base.ValueChanged -= this.ValueChangedDelta;//Remove Delegate
             }
+            else
+            {
+                this.IsCompleted = false;
+            }
         }
+
+        private void ValueChangedByKeyboardCore(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            if (this.IsStarted) return;
+            if (base.FocusState == FocusState.Unfocused) return;
+
+            this.ValueChangedUnfocused?.Invoke(sender, e);//Delegate
+        }
+
     }
 }
